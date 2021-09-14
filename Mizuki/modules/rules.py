32a7114e08f2cd7ba80eab0f7a19fd2c60eca258
MrC《》VENOM <77907590+nandhunair1@users.noreply.github.com>
2021-09-14 10:36:19 +0530

    Update rules.py

--- Mizuki/modules/rules.py
+++ Mizuki/modules/rules.py
@@ -1,18 +1,26 @@
 from typing import Optional
 
-import Mizuki.modules.sql.rules_sql as sql
-from Mizuki import dispatcher
-from Mizuki.modules.helper_funcs.chat_status import user_admin
-from Mizuki.modules.helper_funcs.string_handling import markdown_parser
-from telegram import (InlineKeyboardButton, InlineKeyboardMarkup, Message,
-                      ParseMode, Update, User)
+from telegram import (
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    Message,
+    ParseMode,
+    Update,
+    Bot,
+    User,
+)
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
 from telegram.utils.helpers import escape_markdown
 
+import Mizuki.modules.sql.rules_sql as sql
+from Mizuki import dispatcher
+from Mizuki.modules.helper_funcs.chat_status import user_admin
+from Mizuki.modules.helper_funcs.string_handling import markdown_parser
+
 
 @run_async
-def get_rules(update: Update, context: CallbackContext):
+def get_rules(bot: Bot, update: Update):
     chat_id = update.effective_chat.id
     send_rules(update, chat_id)
 
@@ -28,73 +36,78 @@ def send_rules(update, chat_id, from_pm=False):
             bot.send_message(
                 user.id,
                 "The rules shortcut for this chat hasn't been set properly! Ask admins to "
-                "fix this.")
+                "fix this.",
+            )
             return
         else:
             raise
 
     rules = sql.get_rules(chat_id)
-    text = f"The rules for *{escape_markdown(chat.title)}* are:\n\n{rules}"
+    text = "The rules for *{}* are:\n\n{}".format(escape_markdown(chat.title), rules)"
 
     if from_pm and rules:
         bot.send_message(
-            user.id,
-            text,
-            parse_mode=ParseMode.MARKDOWN,
-            disable_web_page_preview=True)
+            user.id, text, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True
+        )
     elif from_pm:
         bot.send_message(
             user.id,
             "The group admins haven't set any rules for this chat yet. "
-            "This probably doesn't mean it's lawless though...!")
+            "This probably doesn't mean it's lawless though...!",
+        )
     elif rules:
         update.effective_message.reply_text(
             "Contact me in PM to get this group's rules.",
-            reply_markup=InlineKeyboardMarkup([[
-                InlineKeyboardButton(
-                    text="Rules", url=f"t.me/{bot.username}?start={chat_id}")
-            ]]))
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            text="Rules", url=f"t.me/{bot.username}?start={chat_id}"
+                        )
+                    ]
+                ]
+            ),
+        )
     else:
         update.effective_message.reply_text(
             "The group admins haven't set any rules for this chat yet. "
-            "This probably doesn't mean it's lawless though...!")
+            "This probably doesn't mean it's lawless though...!"
+        )
 
 
 @run_async
 @user_admin
-def set_rules(update: Update, context: CallbackContext):
+def set_rules(bot: Bot, update: Update):
     chat_id = update.effective_chat.id
     msg = update.effective_message  # type: Optional[Message]
     raw_text = msg.text
-    args = raw_text.split(None,
-                          1)  # use python's maxsplit to separate cmd and args
+    args = raw_text.split(None, 1)  # use python's maxsplit to separate cmd and args
     if len(args) == 2:
         txt = args[1]
-        offset = len(txt) - len(
-            raw_text)  # set correct offset relative to command
+        offset = len(txt) - len(raw_text)  # set correct offset relative to command
         markdown_rules = markdown_parser(
-            txt, entities=msg.parse_entities(), offset=offset)
+            txt, entities=msg.parse_entities(), offset=offset
+        )
 
         sql.set_rules(chat_id, markdown_rules)
-        update.effective_message.reply_text(
-            "Successfully set rules for this group.")
+        update.effective_message.reply_text("Successfully set rules for this group.")
 
 
 @run_async
 @user_admin
-def clear_rules(update: Update, context: CallbackContext):
+def clear_rules(bot: Bot, update: Update):
     chat_id = update.effective_chat.id
     sql.set_rules(chat_id, "")
     update.effective_message.reply_text("Successfully cleared rules!")
 
 
 def __stats__():
-    return f"{sql.num_chats()} chats have rules set."
+    return "{} chats have rules set.".format(sql.num_chats())
 
 
 def __import_data__(chat_id, data):
     # set chat rules
-    rules = data.get('info', {}).get('rules', "")
+    rules = data.get("info", {}).get("rules", "")
     sql.set_rules(chat_id, rules)
 
 
@@ -103,7 +116,7 @@ def __migrate__(old_chat_id, new_chat_id):
 
 
 def __chat_settings__(chat_id, user_id):
-    return f"This chat has had it's rules set: `{bool(sql.get_rules(chat_id))}`"
+    return "This chat has had it's rules set: `{}`".format(bool(sql.get_rules(chat_id)))
 
 
 __help__ = """
