84b716f8016053ea96600c939f1d1b6538edc745
MrC《》VENOM <77907590+nandhunair1@users.noreply.github.com>
2021-11-28 14:52:26 +0530

    Update cust_filters.py

--- Mizuki/modules/cust_filters.py
+++ Mizuki/modules/cust_filters.py
@@ -601,30 +601,31 @@ def __chat_settings__(chat_id, user_id):
 
 
 __help__ = """
- • `/filters`*:* List all active filters saved in the chat.
+ • /filters : List all active filters saved in the chat.
 
 *Admin only:*
- • `/filter <keyword> <reply message>`*:* Add a filter to this chat. The bot will now reply that message whenever 'keyword'\
+ • /filter <keyword> <reply message> : Add a filter to this chat. The bot will now reply that message whenever 'keyword'\
 is mentioned. If you reply to a sticker with a keyword, the bot will reply with that sticker. NOTE: all filter \
 keywords are in lowercase. If you want your keyword to be a sentence, use quotes. eg: /filter "hey there" How you \
 doin?
  Separate diff replies by `%%%` to get random replies
  *Example:* 
- `/filter "filtername"
+ /filter "filtername"
  Reply 1
  %%%
  Reply 2
  %%%
  Reply 3`
- • `/stop <filter keyword>`*:* Stop that filter.
+ • /stop <filter keyword> : Stop that filter.
 
 *Chat creator only:*
- • `/removeallfilters`*:* Remove all chat filters at once.
+ • /removeallfilters : Remove all chat filters at once.
 
 *Note*: Filters also support markdown formatters like: {first}, {last} etc.. and buttons.
-Check `/markdownhelp` to know more!
+Check /markdownhelp to know more!
 
-"""
+
+© @MrCVENOMbot 😎"""
 
 __mod_name__ = "Filters 📃"
 
