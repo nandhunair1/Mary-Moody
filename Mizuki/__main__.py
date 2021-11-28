7f86c1db89d0fab51b617f9b7b51d656e2e8292a
MrC《》VENOM <77907590+nandhunair1@users.noreply.github.com>
2021-11-28 17:00:14 +0530

    Update __main__.py

--- Mizuki/__main__.py
+++ Mizuki/__main__.py
@@ -95,7 +95,7 @@ Hey there! My name is *{}*.
     "" if not ALLOW_EXCL else "\nAll commands can either be used with / or !\n",
 )
 
-SAITAMA_IMG = "https://te.legra.ph/file/b8628b19a799aea157726.jpg"
+SAITAMA_IMG = "https://te.legra.ph/file/5349a29197507604c7354.jpg"
 
 DONATE_STRING = """Heya, glad to hear you want to donate for developer. This bot runs on heroku so bot slow down some times and developer cannot add more modules due to heroku can't run them.\n\nBetter if my developer recieved a VPS to run the bot. Contact him and help him to continue this.\n\nDeveloper: [MrC_VENOM](t.me/MrC_VENOM)"""
 
