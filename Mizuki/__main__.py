e8d307e00720acad140866b4aea1138d0aa1fedc
MrC《》VENOM <77907590+nandhunair1@users.noreply.github.com>
2021-11-28 11:48:04 +0530

    Update __main__.py

--- Mizuki/__main__.py
+++ Mizuki/__main__.py
@@ -234,10 +234,6 @@ def start(update: Update, context: CallbackContext):
                             InlineKeyboardButton(
                                 text="🇮🇳 Developer", url="https://t.me/MrC_VENOM",
                             ),
-                            InlineKeyboardButton(
-                                text="𝙷𝙴𝙻𝙿",
-                                callback_data="help_back"
-                            ),
                         ],
                     ]
                 ),
