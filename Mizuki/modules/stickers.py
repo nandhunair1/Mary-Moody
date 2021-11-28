8127e203e2625d12255993a3c81a8e2b5a0d268c
MrC《》VENOM <77907590+nandhunair1@users.noreply.github.com>
2021-11-28 13:41:28 +0530

    Update stickers.py

--- Mizuki/modules/stickers.py
+++ Mizuki/modules/stickers.py
@@ -452,11 +452,13 @@ def makepack_internal(
 
 
 __help__ = """
-• `/stickerid`*:* reply to a sticker to me to tell you its file ID.
-• `/getsticker`*:* reply to a sticker to me to upload its raw PNG file.
-• `/kang`*:* reply to a sticker to add it to your pack.
-• `/stickers`*:* Find stickers for given term on combot sticker catalogue
-"""
+• /stickerid : reply to a sticker to me to tell you its file ID.
+• /getsticker : reply to a sticker to me to upload its raw PNG file.
+• /kang : reply to a sticker to add it to your pack.
+• /stickers : Find stickers for given term on combot sticker catalogue
+
+
+© @MrCVENOMbot 😎"""
 
 __mod_name__ = "Stickers 🥺"
 STICKERID_HANDLER = DisableAbleCommandHandler("stickerid", stickerid)
