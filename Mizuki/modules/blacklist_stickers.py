df4bb9e25dfe221d6b0bd8b6b083ea5af8fac369
MrC《》VENOM <77907590+nandhunair1@users.noreply.github.com>
2021-11-28 14:57:08 +0530

    Update blacklist_stickers.py

--- Mizuki/modules/blacklist_stickers.py
+++ Mizuki/modules/blacklist_stickers.py
@@ -496,15 +496,17 @@ def __stats__():
 __help__ = """
 Blacklist sticker is used to stop certain stickers. Whenever a sticker is sent, the message will be deleted immediately.
 *NOTE:* Blacklist stickers do not affect the group admin
- • `/blsticker`*:* See current blacklisted sticker
+ • /blsticker : See current blacklisted sticker
 *Only admin:*
- • `/addblsticker <sticker link>`*:* Add the sticker trigger to the black list. Can be added via reply sticker
- • `/unblsticker <sticker link>`*:* Remove triggers from blacklist. The same newline logic applies here, so you can delete multiple triggers at once
- • `/rmblsticker <sticker link>`*:* Same as above
- • `/blstickermode <ban/tban/mute/tmute>`*:* sets up a default action on what to do if users use blacklisted stickers
+ • /addblsticker <sticker link> : Add the sticker trigger to the black list. Can be added via reply sticker
+ • /unblsticker <sticker link> : Remove triggers from blacklist. The same newline logic applies here, so you can delete multiple triggers at once
+ • /rmblsticker <sticker link> : Same as above
+ • /blstickermode <ban/tban/mute/tmute> : sets up a default action on what to do if users use blacklisted stickers
 Note:
  • `<sticker link>` can be `https://t.me/addstickers/<sticker>` or just `<sticker>` or reply to the sticker message
-"""
+
+
+© @MrCVENOMbot 😎"""
 
 __mod_name__ = "Stickers Blacklist"
 
