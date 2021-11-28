e8c48e3f384740f218b22ae1bf639d8553dbd935
MrC《》VENOM <77907590+nandhunair1@users.noreply.github.com>
2021-11-28 15:03:40 +0530

    Update antiflood.py

--- Mizuki/modules/antiflood.py
+++ Mizuki/modules/antiflood.py
@@ -403,12 +403,12 @@ Antiflood allows you to take action on users that send more than x messages in a
 will result in restricting that user.
 
  This will mute users if they send more than 10 messages in a row, bots are ignored.
- • `/flood`*:* Get the current flood control setting
+ • /flood : Get the current flood control setting
 
 • *Admins only:*
- • `/setflood <int/'no'/'off'>`*:* enables or disables flood control
+ • /setflood <int/'no'/'off'> : enables or disables flood control
  *Example:* `/setflood 10`
- • `/setfloodmode <ban/kick/mute/tban/tmute> <value>`*:* Action to perform when user have exceeded flood limit. ban/kick/mute/tmute/tban
+ • /setfloodmode <ban/kick/mute/tban/tmute> <value> : Action to perform when user have exceeded flood limit. ban/kick/mute/tmute/tban
 
 • *Note:*
  • Value must be filled for tban and tmute!!
@@ -417,7 +417,9 @@ will result in restricting that user.
  `6h` = 6 hours
  `3d` = 3 days
  `1w` = 1 week
- """
+
+
+© @MrCVENOMbot 😎"""
 
 __mod_name__ = "Anti-Flood 🚫"
 
