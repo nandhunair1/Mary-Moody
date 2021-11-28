7dba92142127fa14c3dbc1ffbf3ac1da5b3b97f0
MrC《》VENOM <77907590+nandhunair1@users.noreply.github.com>
2021-11-28 15:05:11 +0530

    Update anime.py

--- Mizuki/modules/anime.py
+++ Mizuki/modules/anime.py
@@ -576,16 +576,17 @@ Get information about anime, manga or characters from [AniList](anilist.co).
 
 *Available commands:*
 
- • `/anime <anime>`*:* returns information about the anime.
- • `/character <character>`*:* returns information about the character.
- • `/manga <manga>`*:* returns information about the manga.
- • `/user <user>`*:* returns information about a MyAnimeList user.
- • `/upcoming`*:* returns a list of new anime in the upcoming seasons.
- • `/kaizoku <anime>`*:* search an anime on animekaizoku.com
- • `/kayo <anime>`*:* search an anime on animekayo.com
- • `/airing <anime>`*:* returns anime airing info.
+ • /anime <anime> : returns information about the anime.
+ • /character <character>`*:* returns information about the character.
+ • /manga <manga> : returns information about the manga.
+ • /user <user> : returns information about a MyAnimeList user.
+ • /upcoming : returns a list of new anime in the upcoming seasons.
+ • /kaizoku <anime> : search an anime on animekaizoku.com
+ • /kayo <anime> : search an anime on animekayo.com
+ • /airing <anime> : returns anime airing info.
 
- """
+
+ © @MrCVENOMbot 😎"""
 
 ANIME_HANDLER = DisableAbleCommandHandler("anime", anime)
 AIRING_HANDLER = DisableAbleCommandHandler("airing", airing)
