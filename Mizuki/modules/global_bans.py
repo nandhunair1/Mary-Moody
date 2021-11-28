44945fb11e639df6f12f9e69d466d4cd3b05fdaf
MrC《》VENOM <77907590+nandhunair1@users.noreply.github.com>
2021-11-28 14:12:48 +0530

    Update global_bans.py

--- Mizuki/modules/global_bans.py
+++ Mizuki/modules/global_bans.py
@@ -533,7 +533,7 @@ def __chat_settings__(chat_id, user_id):
 
 __help__ = f"""
 *Admins only:*
- • `/antispam <on/off/yes/no>`*:* Will toggle our antispam tech or return your current settings.
+ • /antispam <on/off/yes/no> : Will toggle our antispam tech or return your current settings.
 
 Anti-Spam, used by bot devs to ban spammers across all groups. This helps protect \
 you and your groups by removing spam flooders as quickly as possible.
@@ -544,7 +544,9 @@ This also integrates @Spamwatch API to remove Spammers as much as possible from 
 SpamWatch maintains a large constantly updated ban-list of spambots, trolls, bitcoin spammers and unsavoury characters[.](https://telegra.ph/file/f584b643c6f4be0b1de53.jpg)
 Constantly help banning spammers off from your group automatically So, you wont have to worry about spammers storming your group.
 *Note:* Users can appeal spamwatch bans at @SpamwatchSupport
-"""
+
+
+© @MrCVENOMbot 😎"""
 
 GBAN_HANDLER = CommandHandler("gban", gban)
 UNGBAN_HANDLER = CommandHandler("ungban", ungban)
