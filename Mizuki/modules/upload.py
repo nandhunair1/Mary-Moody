d40710eca443e7923282988058771717a96cd8e5
MrC《》VENOM <77907590+nandhunair1@users.noreply.github.com>
2021-11-28 13:32:44 +0530

    Update upload.py

--- Mizuki/modules/upload.py
+++ Mizuki/modules/upload.py
@@ -83,7 +83,7 @@ async def tsh(event):
 
             str(time.time() - start)
             await orta.edit(
-                f"File Successfully Uploaded to TransferSh.\n\nLink 👉 {download_link}\nExpired Date 👉 {final_date}\n\nUploaded by @ain1bot 😎"
+                f"File Successfully Uploaded to TransferSh.\n\nLink 👉 {download_link}\nExpired Date 👉 {final_date}\n\nUploaded by © @MrCVENOMbot 😎"
             )
         except Exception as e:
             traceback.print_exc()
@@ -118,7 +118,7 @@ async def tmp(event):
 
             str(time.time() - start)
             await orta.edit(
-                f"File Successfully Uploaded to TmpNinja.\n\nLink 👉 {download_link}\n\nUploaded by @ain1bot 😎"
+                f"File Successfully Uploaded to TmpNinja.\n\nLink 👉 {download_link}\n\nUploaded by © @MrCVENOMbot 😎"
             )
         except Exception as e:
             traceback.print_exc()
@@ -181,10 +181,10 @@ if __name__ == "__main__":
     main()
 
 __help__ = """
- ⦁ `/transfersh`*:* reply to a telegram file to upload it on transfersh and get direct download link
- ⦁ `/tmpninja`*:* reply to a telegram file to upload it on tmpninja and get direct download link
+ ⦁ /transfersh : reply to a telegram file to upload it on transfersh and get direct download link
+ ⦁ /tmpninja : reply to a telegram file to upload it on tmpninja and get direct download link
 
-© @ain1bot 😎
+© @MrCVENOMbot 😎
 """
 
 __mod_name__ = "File To Link 🔗"
