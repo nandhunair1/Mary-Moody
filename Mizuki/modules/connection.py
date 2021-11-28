5fcdf640bd8387855be5b5bd2b2e380a2ba332a2
MrC《》VENOM <77907590+nandhunair1@users.noreply.github.com>
2021-11-28 14:54:19 +0530

    Update connection.py

--- Mizuki/modules/connection.py
+++ Mizuki/modules/connection.py
@@ -393,14 +393,16 @@ __help__ = """
 Sometimes, you just want to add some notes and filters to a group chat, but you don't want everyone to see; This is where connections come in...
 This allows you to connect to a chat's database, and add things to it without the commands appearing in chat! For obvious reasons, you need to be an admin to add things; but any member in the group can view your data.
 
- • `/connect`: Connects to chat (Can be done in a group by `/connect` or `/connect` <chat id> in PM)
- • `/connection`: List connected chats
- • `/disconnect`: Disconnect from a chat
- • `/helpconnect`: List available commands that can be used remotely
+ • /connect : Connects to chat (Can be done in a group by `/connect` or `/connect` <chat id> in PM)
+ • /connection : List connected chats
+ • /disconnect : Disconnect from a chat
+ • /helpconnect : List available commands that can be used remotely
 
 *Admin only:*
- • /allowconnect <yes/no>: allow a user to connect to a chat
-"""
+ • /allowconnect <yes/no> : allow a user to connect to a chat
+
+
+© @MrCVENOMbot 😎"""
 
 CONNECT_CHAT_HANDLER = CommandHandler("connect", connect_chat, pass_args=True)
 CONNECTION_CHAT_HANDLER = CommandHandler("connection", connection_chat)
