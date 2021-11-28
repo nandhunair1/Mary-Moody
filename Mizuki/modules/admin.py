37745939dc05978e7aade0cbb78b1bfa0f4130ee
MrC《》VENOM <77907590+nandhunair1@users.noreply.github.com>
2021-11-28 15:08:08 +0530

    Update admin.py

--- Mizuki/modules/admin.py
+++ Mizuki/modules/admin.py
@@ -464,17 +464,19 @@ def adminlist(update, context):
 
 
 __help__ = """
- • `/admins`*:* list of admins in the chat
+ • /admins : list of admins in the chat
 
 *Admins only:*
- • `/pin`*:* silently pins the message replied to - add `'loud'` or `'notify'` to give notifs to users
- • `/unpin`*:* unpins the currently pinned message
- • `/invitelink`*:* gets invitelink
- • `/promote`*:* promotes the user replied to
- • `/demote`*:* demotes the user replied to
- • `/title <title here>`*:* sets a custom title for an admin that the bot promoted
- • `/admincache`*:* force refresh the admins list
-"""
+ • /pin : silently pins the message replied to - add `'loud'` or `'notify'` to give notifs to users
+ • /unpin : unpins the currently pinned message
+ • /invitelink : gets invitelink
+ • /promote : promotes the user replied to
+ • /demote : demotes the user replied to
+ • /title <title here> : sets a custom title for an admin that the bot promoted
+ • /admincache : force refresh the admins list
+
+
+© @MrCVENOMbot 😎"""
 
 ADMINLIST_HANDLER = DisableAbleCommandHandler("admins", adminlist)
 
