ea570f52dedbc98fc51b0fd54b9248f00f372e5d
MrC《》VENOM <77907590+nandhunair1@users.noreply.github.com>
2021-11-28 13:04:48 +0530

    Update youtube.py

--- Mizuki/modules/youtube.py
+++ Mizuki/modules/youtube.py
@@ -131,6 +131,6 @@ async def download_video(v_url):
 
 
 __help__ = """
- • `/ytaudio <link>` or `/ytvideo <link>`*:* Downlods a video or audio from a youtube video to the bots local server and uploads to telegram
+ • ytaudio <link> or /ytvideo <link> : Downlods a video or audio from a youtube video to the bots local server and uploads to telegram
 """
 __mod_name__ = "YouTube 🎬"
