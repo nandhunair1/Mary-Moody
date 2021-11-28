cc473f4384e13f4a1a9da0b86bab29409b291af2
MrC《》VENOM <77907590+nandhunair1@users.noreply.github.com>
2021-11-28 23:19:36 +0530

    Update fun.py

--- Mizuki/modules/fun.py
+++ Mizuki/modules/fun.py
@@ -99,7 +99,7 @@ def roll_dice(client, message):
     rep_mesg_id = message.message_id
     if message.reply_to_message:
         rep_mesg_id = message.reply_to_message.message_id
-    await client.send_dice(
+        await client.send_dice(
         chat_id=message.chat.id,
         emoji=GOAL_E_MOJI,
         disable_notification=True,
